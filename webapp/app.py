4df33bbadee0d0c00aa87bccb194265e1a6194ef
nithyananthantechy <nithyananthannagarajan092@gmail.com>
2026-04-04 12:53:06 +0530

    login page logo issue fix

--- webapp/app.py
+++ webapp/app.py
@@ -281,4 +281,4 @@ def ping():
 # ─── MAIN ─────────────────────────────────────────────────────────────────────
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
